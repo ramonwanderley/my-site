45d24dcc92a2c8f57b76a75b356537e195674144
Michael Barney <gatomaiquinho@gmail.com>
2017-11-01 11:27:46 +0000

    weather updated

--- fcc-portfolio/weather/js/index.js
+++ fcc-portfolio/weather/js/index.js
@@ -1,6 +1,6 @@
 var unit = 0; //0 = C; 1 = F
 var temp = 25;
-
+var city;
 $("#button-holder").on("click", function(){
     if($(this).css("text-align") == "left"){
         $(this).css("text-align", "right");
@@ -14,7 +14,27 @@ $("#button-holder").on("click", function(){
 })
 
 window.onload = function(){
-    setTemp(temp, unit);
+    var lat;
+    var lon;
+    
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
+        $("#data").html("latitude: " + lat  + "<br>longitude: " + lon);
+        
+        $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat="+ lat + "&lon=" + lon, function(json){
+            city = json["name"];
+            temp = Math.round(json["main"]["temp"]);
+            var main = json["weather"]["main"];
+            var description = json["weather"][0]["description"];
+            
+            setTemp(temp, unit);
+            $("#city").text(city);
+            $("#weather").text(description);
+        });
+      });
+    }
 }
 
 function setTemp(celcius, unit){
