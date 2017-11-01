49fb9169287474e11ca3daa367736597520bcfe2
Michael Barney <gatomaiquinho@gmail.com>
2017-11-01 17:32:48 +0000

    finished weather app

--- fcc-portfolio/weather/js/index.js
+++ fcc-portfolio/weather/js/index.js
@@ -14,26 +14,44 @@ $("#button-holder").on("click", function(){
 })
 
 window.onload = function(){
-    var lat;
-    var lon;
-    
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        lat = position.coords.latitude;
-        lon = position.coords.longitude;
-        $("#data").html("latitude: " + lat  + "<br>longitude: " + lon);
-        
-        $.getJSON("https://fcc-weather-api.glitch.me/api/current?lat="+ lat + "&lon=" + lon, function(json){
-            city = json["name"];
+    if(getParameterByName("city")){
+        var city =getParameterByName("city");
+        $.getJSON("https://api.openweathermap.org/data/2.5/weather?appid=26e86f6a7a3e1882df78703644334252&units=metric&q="+ city, function(json){
             temp = Math.round(json["main"]["temp"]);
-            var main = json["weather"]["main"];
+            var main = json["weather"][0]["main"];
             var description = json["weather"][0]["description"];
-            
+            var id = json["weather"][0]["id"];
+            $("#w_icon").addClass("owf-"+id)
             setTemp(temp, unit);
             $("#city").text(city);
             $("#weather").text(description);
+            
+            $("#shower").css("display", "block");
+            $("#shower").addClass("bounceIn");
         });
-      });
+    }
+    
+    else{
+        var lat;
+        var lon;
+    
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            lat = position.coords.latitude;
+            lon = position.coords.longitude;
+            $.getJSON("https://api.openweathermap.org/data/2.5/weather?appid=26e86f6a7a3e1882df78703644334252&units=metric&lat="+ lat + "&lon=" + lon, function(json){
+                city = json["name"];
+                temp = Math.round(json["main"]["temp"]);
+                var main = json["weather"][0]["main"];
+                var description = json["weather"][0]["description"];
+                var id = json["weather"][0]["id"];
+                $("#w_icon").addClass("owf-"+id)
+                setTemp(temp, unit);
+                $("#city").text(city);
+                $("#weather").text(description);
+            });
+          });
+        }
     }
 }
 
@@ -41,8 +59,18 @@ function setTemp(celcius, unit){
     var mt = celcius;
     var u = "°C";
     if(unit == 1){
-        mt= celcius*1.8 + 32;
+        mt= Math.round(celcius*1.8 + 32);
         u = "°F";
     }
     $("#temp").text(mt+u);
+}
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
